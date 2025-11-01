a0adf37e958b0de9e7b8214bb94908363e6d6567
Code Base Mac Mini <code@base.com>
2025-11-01 22:35:10 +0100

    Cuarto commit

--- api.php
+++ api.php
@@ -17,12 +17,12 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json; charset=UTF-8");
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+$method = $_SERVER['REQUEST_METHOD'];
+
+if ($method === 'OPTIONS') {
     http_response_code(204);
     exit;
 }
-
-$method = $_SERVER['REQUEST_METHOD'];
 
 try {
     switch ($method) {
